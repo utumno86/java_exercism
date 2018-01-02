2fdc6242342c9d26b07221e2d05aaef13b7c4c99
Michael Nash <michael@spatialnetworks.com>
2018-01-02 17:42:39 -0500

    reverse-string work in progress

--- reverse-string/src/main/java/ReverseString.java
+++ reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,15 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        return inputString;
+        if (inputString == ""){
+          return "";
+        }
+        char[] chars = inputString.toCharArray();
+        String outputString;
+        for (int i = chars.length; i > 0; i--){
+          System.out.println Character.toString(chars[i]);
+        }
+        return "tobor";
     }
 
 }
