5e87bd682457464424618fab71bd9462def3424d
Michael Nash <michael@spatialnetworks.com>
2019-07-08 12:09:47 -0400

    fallthrough cases'

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -4,55 +4,36 @@ class Scrabble {
     private int getLetterScore(char letter) {
         switch(letter){
             case 'A':
-                return 1;
             case 'E':
-                return 1;
             case 'I':
-                return 1;
             case 'O':
-                return 1;
             case 'U':
-                return 1;
             case 'L':
-                return 1;
             case 'N':
-                return 1;
             case 'R':
-                return 1;
             case 'S':
-                return 1;
             case 'T':
                 return 1;
             case 'D':
-                return 2;
             case 'G':
                 return 2;
             case 'B':
-                return 3;
             case 'C':
-                return 3;
             case 'M':
-                return 3;
             case 'P':
                 return 3;
             case 'F':
-                return 4;
             case 'H':
-                return 4;
             case 'V':
-                return 4;
             case 'W':
-                return 4;
             case 'Y':
                 return 4;
             case 'K':
                 return 5;
             case 'J':
-                return 8;
             case 'X':
                 return 8;
             case 'Q':
-                return 10;
             case 'Z':
                 return 10;
             default:
