cf6376f2b8b0922e40dc366b742f11163f6daaf0
Michael Nash <michael@spatialnetworks.com>
2019-08-21 13:24:57 -0400

    refactored square into a seperate function

--- difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -5,19 +5,23 @@ class DifferenceOfSquaresCalculator {
         for (int i = 1; i <= input; i++){
             sum = sum + i;
         };
-        return (int) (Math.pow(sum, 2));
+        return square(sum);
     }
 
     int computeSumOfSquaresTo(int input) {
         int sum = 0;
         for (int i = 1; i <= input; i++){
-            sum = sum + (int) (Math.pow(i, 2));
+            sum = sum + square(i);
         };
         return sum;
     }
 
     int computeDifferenceOfSquares(int input) {
        return this.computeSquareOfSumTo(input) - this.computeSumOfSquaresTo(input);
+    }
+
+    int square(int i) {
+        return i * i;
     }
 
 }
